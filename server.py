7ddc2afca4e0ca7e922c1e8fbb3158f755871e44
John Zakkam <ced18i059@iiitdm.ac.in>
2020-12-07 02:27:09 +0530

    upd

--- server.py
+++ server.py
@@ -88,7 +88,7 @@ def threaded(c,addr):
             break
     if(flag==0): iptable.append([addr,(ip,addr[1])])
     print_lock.release()
-    info=main(ip)
+    info=main(addr[0])
     c.send(str(info).encode())
     c.close()
     splitip()
